1eceb35304102287e2c0229e3c4d04ff3a962078
yonatan <yonatan5556201@gmail.com>
2024-05-12 23:11:22 +0300

    bb

--- hw3.py
+++ hw3.py
@@ -35,7 +35,6 @@ def render_scene(camera, ambient, lights, objects: list[Object3D], screen_size, 
     width, height = screen_size
     ratio = float(width) / height
     screen = (-1, 1 / ratio, 1, -1 / ratio)  # left, top, right, bottom
-    light = lights[0]
     image = np.zeros((height, width, 3))
 
     for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
@@ -49,15 +48,15 @@ def render_scene(camera, ambient, lights, objects: list[Object3D], screen_size, 
 
             intersection_point = camera_ray.origin + intersection_distance * camera_ray.direction
             intersection_point += nearest_object.normal * 1e-5
-
-            light_ray = light.get_light_ray(intersection_point)
-            blocker, _ = light_ray.nearest_intersected_object(objects)
-            if blocker is not None:
-                color = np.zeros(3)
-            else:
-                light_vector = normalize(light_ray.direction)
-                light_intensity = light.get_intensity(intersection_point)
-                color = nearest_object.phong_color(nearest_object.normal, light_vector, camera_ray.direction, light_intensity, ambient)
+            color = np.zeros(3)
+            for light in lights:
+                light_ray = light.get_light_ray(intersection_point)
+                blocker, _ = light_ray.nearest_intersected_object(objects)
+                if blocker is None:
+                    light_vector = normalize(light_ray.direction)
+                    light_intensity = light.get_intensity(intersection_point)
+                    color += nearest_object.phong_color(nearest_object.normal, light_vector, camera_ray.direction,
+                                                        light_intensity, ambient)
 
             # This is the main loop where each pixel color is computed.
             image[i, j] = color
